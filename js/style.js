da1bc5ac0dfd9f04edc87446564a9ebf4db9260a
nguyen.thi.net <FRAMGIA\nguyen.thi.net@b121787.framgia.com>
2019-03-08 14:09:37 +0700

    first commit

---
+++ js/style.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    var modal = $("#myModal");
+    var btn = $("#myBtn");
+    var close = $(".close");
+
+    btn.click(function () {
+        modal.show();
+        $(".modal-dialog").animate({
+            top: "15%",
+            opacity : '1'
+        });
+    })
+
+    close.click(function () {
+        modal.hide();
+        $(".modal-dialog").animate({
+            top: "-100%",
+            opacity : '0'
+        });
+    })
+ 
+    $(this).click(function (event) {           
+        if (!$(event.target).closest(".modal-dialog, #myBtn").length) {
+            $("body").find(".modal").css('display',"none");
+            $("body").find(".modal-dialog").animate({
+                top: "-100%",
+                opacity : '0'
+            });
+            
+        }
+    });
+
+});
